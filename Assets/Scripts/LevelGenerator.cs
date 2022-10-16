40a2ee7e4dacdd4698422e08380b3c8d48db1ed0
renzobptrt <renzobptrt@gmail.com>
2022-10-16 02:24:37 -0500

    PowerupsInventory in MainMenu

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -76,7 +76,4 @@ public class LevelGenerator : MonoBehaviour
             AddLevelBlock();
         }
     }
-
-
-
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,12 @@ public class MainMenu : MonoBehaviour
     public Button buttonStartGame = null;
     public Button buttonQuitGame = null;
     public Button buttonViewInventory = null;
+    public Button buttonBackMainMenu = null;
+
+    public RectTransform rtMainPanel = null;
+    public RectTransform rtPowerupsPanel = null;
+
+    public List<Button> buttonsPowerups = new List<Button>();
 
     void Start()
     {
@@ -29,6 +35,29 @@ public class MainMenu : MonoBehaviour
         {
             buttonViewInventory.StopAllCoroutines();
             //Mostrar GUI Inventario Powerups
+            buttonViewInventory.onClick.AddListener(() => {
+                rtMainPanel.anchoredPosition = new Vector2(2000, 0);
+                rtPowerupsPanel.anchoredPosition = Vector2.zero;
+            });
+        }
+
+        if(buttonBackMainMenu != null)
+        {
+            buttonBackMainMenu.StopAllCoroutines();
+            buttonBackMainMenu.onClick.AddListener(() => {
+                rtMainPanel.anchoredPosition = Vector2.zero; 
+                rtPowerupsPanel.anchoredPosition = new Vector2(2000, 0);
+            });
+        }
+
+        if(buttonsPowerups.Count > 0)
+        {
+            for(int i=0; i<buttonsPowerups.Count; i++)
+            {
+                int temp = i;
+                buttonsPowerups[temp].StopAllCoroutines();
+                buttonsPowerups[temp].onClick.AddListener(() => SetPowerups(temp));
+            }
         }
     }
 
@@ -46,5 +75,15 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("GameLevel");
     }
 
+    private void SetPowerups(int index)
+    {
+        Image currentImage = buttonsPowerups[index].transform.GetChild(0).GetComponent<Image>();
+        Color currentColor = currentImage.color;
 
+        int setCurrentState = currentColor == Color.white ? 0 : 1;
+        string currentPowerupIndex = "Powerup_" + index;
+        PlayerPrefs.SetInt(currentPowerupIndex, setCurrentState);
+
+        currentImage.color = setCurrentState == 0 ? Color.black : Color.white; 
+    }
 }
